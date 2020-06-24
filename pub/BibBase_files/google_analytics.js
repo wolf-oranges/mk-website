7e66459de4fef7f56128492080a12b25a3ebc731
Mia Truman <miatrutru@gmail.com>
2020-06-24 09:58:33 -0700

    stuff?

---
+++ pub/BibBase_files/google_analytics.js
@@ -0,0 +1,9 @@
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-37729657-1', 'auto');
+// _gaq.push(['_setDomainName', 'bibbase.org']);
+// https://developers.google.com/analytics/devguides/collection/analyticsjs/domains#auto
+ga('require', 'displayfeatures');
+ga('require', 'linkid', 'linkid.js');
+ga('send', 'pageview');
